5f3655d7694f0a75279cbc672f8515186f9823d8
Erwin Balkema <erwinbalkema@macbookpro.home>
2025-03-10 17:35:27 +0100

    Upgrade Firebase Functions naar Node.js 20 en firebase-functions v5

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,22 +1,14 @@
-const functions = require('firebase-functions');
+const { onRequest } = require('firebase-functions/v2/https');
 const fetch = require('node-fetch');
 
 /**
  * Proxy voor weerafbeeldingen van OpenWeatherMap
  * Deze functie lost CORS-problemen op door de afbeeldingen via de server te laden
  */
-exports.weatherIcon = functions.https.onRequest(async (req, res) => {
-  // CORS headers
-  res.set('Access-Control-Allow-Origin', '*');
-  res.set('Access-Control-Allow-Methods', 'GET');
-  res.set('Access-Control-Allow-Headers', 'Content-Type');
-  
-  // Preflight request
-  if (req.method === 'OPTIONS') {
-    res.status(204).send('');
-    return;
-  }
-  
+exports.weatherIcon = onRequest({
+  cors: true,
+  maxInstances: 10
+}, async (req, res) => {
   // Controleer of er een icon parameter is
   const iconCode = req.query.icon;
   if (!iconCode) {
