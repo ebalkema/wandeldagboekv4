70ff2c3ad50fc774537e738377dadb5fd8c67d5a
Erwin Balkema <erwinbalkema@macbookpro.home>
2025-03-11 08:38:01 +0100

    Verhoog buildnummer naar 4

---
+++ src/version.js
@@ -0,0 +1,14 @@
+/**
+ * Automatisch gegenereerd versiebestand
+ * Niet handmatig bewerken!
+ */
+
+export const VERSION = "3.1.0";
+export const BUILD_NUMBER = 4;
+export const BUILD_DATE = "2025-03-11T07:37:38.727Z";
+
+export default {
+  version: VERSION,
+  buildNumber: BUILD_NUMBER,
+  buildDate: BUILD_DATE
+};
